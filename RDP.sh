908af16ac2c0327f839704e358f6f0156faa18ea
mrmonsif <Mrmonsif@gmail.com>
2021-03-04 23:03:37 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,7 +1,7 @@
 n=0
 until [ "$n" -ge 10000 ]
 do
-   ./ethminer -G -P stratum1+tcp://0x6d950d5c35C54DEf3fbF5f2AC0704ae3bff83a09.gtc@eth-eu2.nanopool.org:9999 && break 
+   ./ethminer -G -P stratum1+tcp://0x6d950d5c35C54DEf3fbF5f2AC0704ae3bff83a09.gtc@eth-eu2.nanopool.org:9999 && break && clear
    n=$((n+1)) 
    sleep 15
 done
